9d15759ace7ddfe48a9f348047cd55a0c02e3cea
yjo0514 <yjo0514@naver.com>
2021-07-23 15:46:51 +0900

    0721_industryUniv

---
+++ script/script.js
@@ -0,0 +1,52 @@
+$(function(){
+
+    // GNB 시작
+    $(".mainLi a, #subMenuBG").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+        $("div#subMenuBG").stop().slideDown(800);
+    });
+    
+    $(".mainLi a, #subMenuBG").mouseout(function(){
+        $(".subMenu").stop().slideUp(300);
+        $("div#subMenuBG").stop().slideUp(300);
+    });
+    // GNB 끝
+
+    // 슬라이드쇼 시작
+    setInterval(fnSlide, 3000);
+
+    function fnSlide() {
+        $("#shuttleFrame").animate(
+            {"margin-left": "-1200px"},
+            2400,
+            function(){
+                $("#slide a:first-child").insertAfter("#slide a:last-child");
+                $("#shuttleFrame").css({"margin-left": "0"});
+            }
+        );
+    }
+
+
+    // 슬라이드쇼 끝
+
+
+    // 모달레이어 팝업 시작
+
+    $("#popPoint").click(function(){
+        $("#layerBG").css({"display":"block"});
+
+    });
+
+        $("#closeBtnArea button").click(function(){
+            $("#layerBG").css({"display":"none"});
+
+        });
+    
+
+
+    // 모달레이어 팝업 끝
+
+
+
+
+});
